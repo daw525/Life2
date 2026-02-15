d97bfe4c730e8f32f248ebe4da73923b4f5f4dd5
David Wilson <wilson479@hotmail.com>
2026-02-15 10:48:41 +0000

    Added initial unit test for region.c

---
+++ Tests/TestRegion.c
@@ -0,0 +1,121 @@
+#include "./Unity/unity.h"
+#include "../Source/region.h"
+#include <string.h>
+
+#define MAX_MESSAGE     (128)
+#define MAX_TEST_CASE    (20)
+
+typedef struct {
+    bool testEnabled;
+    char message[MAX_MESSAGE];
+    region r;
+} testCase;
+
+//void initialiseMapping(mapping *m) {
+
+//}
+
+void initialiseEntity(entity *e) {
+    /* Need to move this into entity.c */
+    int sample;
+    e->input = false;
+    e->output = false;
+    e->previousOutput = false;
+    for(sample=0;sample<MAX_SAMPLE;sample++) {    
+        e->inputs[sample] = NO_SAMPLE;
+        e->outputs[sample] = NO_SAMPLE;
+    }
+    e->INTEGRATION_TIME = 1;
+    e->integrationTime = 0;
+    e->inputTimeTrue = 0;
+    e->outputTimeTrue = 0;
+    e->outputTimeFalse = 0;
+    e->error = 0;
+    e->trueWeight = 0;
+    e->falseWeight = 0;
+    e->FLIP_TIME = 20;
+    e->flipTime = 0;
+    e->firstPass = true; 
+}
+
+void initialiseLayer(layer *l) {
+
+}
+
+void initialiseRegion(region *r) {
+    memset(r,0,sizeof(r));
+}
+
+void initialiseTestCase(testCase *t, char *message) {
+    port tempPort;
+    int currentLayer, currentMapping, currentEntity, currentPort;
+    t->testEnabled=true;
+    sprintf(t->message,message);
+    initialiseRegion(&t->r);
+
+    /* Set up region inputs */
+    t->r.inputCount++;
+
+    /* Add layer */
+    currentLayer = t->r.layerCount;
+    initialiseLayer(&t->r.layers[currentLayer]);
+
+    /* Add mappings */
+    currentMapping = t->r.layers[currentLayer].mappingCount;
+
+    initialiseMapping(&t->r.layers[currentLayer].mappings[currentMapping],THRESHOLD);
+    /* Set input ports */
+    initialisePort(&tempPort,&t->r.inputArray[0],false);
+    addPortToMapping(&t->r.layers[currentLayer].mappings[currentMapping],&tempPort,true);
+   /* Set output port */
+    initialisePort(&tempPort,&t->r.layers[currentLayer].entities[0].input,false);
+    addPortToMapping(&t->r.layers[currentLayer].mappings[currentMapping],&tempPort,false);
+
+    t->r.layers[currentLayer].mappingCount++;
+    currentMapping = t->r.layers[currentLayer].mappingCount;
+    /* Next mapping */
+
+    /* Add entities */
+    currentEntity = t->r.layers[currentLayer].entityCount;
+    initialiseEntity(&t->r.layers[currentLayer].entities[currentEntity]);
+
+    /* Next entity */
+    t->r.layers[currentLayer].entityCount++;
+    currentEntity = t->r.layers[currentLayer].entityCount;
+
+    /* Next layer */
+    t->r.layerCount++;
+    currentLayer = t->r.layerCount;
+
+}
+
+void setUp(void) {
+     
+}
+
+void tearDown(void) {
+
+}
+
+void test_processRegion(void) {
+    testCase tests[MAX_TEST_CASE];
+    int testCase;
+
+    testCase = 0;
+    initialiseTestCase(&tests[testCase],"Setting up test framework for region\0");
+
+    for(testCase=0;testCase<MAX_TEST_CASE;testCase++) {
+        if (tests[testCase].testEnabled == true) {
+
+            processRegion(&tests[testCase].r);
+            printRegionState(&tests[testCase].r);
+            //TEST_ASSERT_EQUAL_MESSAGE(tests[testCase].expectedOutput,tests[testCase].e.output,tests[testCase].message);
+        }
+    }
+}
+
+int main(void) {
+    UNITY_BEGIN();
+    RUN_TEST(test_processRegion);
+    return UNITY_END();
+}
